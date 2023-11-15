7252a1960239900d77d23b7745749f6ebfd4285f
SamehM20 <134065615+SamehM20@users.noreply.github.com>
2023-11-15 10:14:04 +0200

    Update circuit_1_dataflow_1.v

--- Part 1/Codes/circuit_1_dataflow_1.v
+++ Part 1/Codes/circuit_1_dataflow_1.v
@@ -1,9 +1,9 @@
 module circuit_1_dataflow_1 (
-input A, B,
-output Q
+  input A, B,
+  output Q
 );
-wire X, Y; // Declaring the inner nets.
-assign X = A & B;
-assign Y = ~ B;
-assign Q = X | Y;
+  wire X, Y; // Declaring the inner nets.
+  assign X = A & B;
+  assign Y = ~ B;
+  assign Q = X | Y;
 endmodule
