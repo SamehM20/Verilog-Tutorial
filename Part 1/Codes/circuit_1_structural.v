768a53cbc2de6116c149aaace3ceec7a57c5a89e
SamehM20 <134065615+SamehM20@users.noreply.github.com>
2023-11-15 10:15:24 +0200

    Update circuit_1_structural.v

--- Part 1/Codes/circuit_1_structural.v
+++ Part 1/Codes/circuit_1_structural.v
@@ -1,9 +1,9 @@
 module circuit_1_structural (
-input A, B,
-output Q
+  input A, B,
+  output Q
 );
-wire X, Y; // Declaring the inner nets.
-and a0 (X, A, B);
-not n0 (Y, B);
-nor n1 (Q, X, Y);
+  wire X, Y; // Declaring the inner nets.
+  and a0 (X, A, B);
+  not n0 (Y, B);
+  nor n1 (Q, X, Y);
 endmodule
